1b5962c29c823f8cca586c32b4e95ec03be5197a
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-11-23 18:05:14 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  new_array = [] 
+  
+  array.each do |word|
+  new_array << sentence.reverse
+  end 
+new_array.join(" ")
+end 
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  new_array = []
+  
+  array.collect do |sentence|
+  new_array << sentence.reverse 
+  end
+new_array.join(" ") 
+end 
